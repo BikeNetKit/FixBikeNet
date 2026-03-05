f36e6f557349fc674f4d30b3e94dca536e0cc4df
Manuel-Knepper <manuel.knepper@gmx.net>
2026-03-05 15:36:20 +0100

    added test_weigh_edges to test_functions.py

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -45,3 +45,34 @@ def create_validation_edge_list():
 
 def test_find_edges_to_drop(create_parallel_test_graph, create_validation_edge_list):
     assert find_edges_to_drop(create_parallel_test_graph) == create_validation_edge_list
+
+@pytest.fixture
+def create_graph_to_weigh():
+    G = nx.MultiDiGraph()
+    G.add_edges_from([(1, 2), (1, 3), (2, 3)])
+    attributes = {(1, 2, 0): {'length': 10, 'pbi': True},
+                  (1, 3, 0): {'length': 4, 'pbi': False},
+                  (2, 3, 0): {'length': 5, 'pbi': True}}
+    nx.set_edge_attributes(G, attributes)
+    return G
+
+@pytest.fixture
+def create_weighted_graph():
+    G = nx.MultiDiGraph()
+    G.add_edges_from([(1, 2), (1, 3), (2, 3)])
+    attributes = {(1, 2, 0): {'length': 10, 'pbi': True, 'weight': 10},
+                  (1, 3, 0): {'length': 4, 'pbi': False, 'weight': 20},
+                  (2, 3, 0): {'length': 5, 'pbi': True, 'weight': 5}}
+    nx.set_edge_attributes(G, attributes)
+    return G
+
+@pytest.fixture
+def create_penalty():
+    penalty = {
+        0: 5,
+        1: 1
+    }
+    return penalty
+
+def test_weigh_edges(create_graph_to_weigh, create_weighted_graph, create_penalty):
+    assert graphs_equal(weigh_edges(create_graph_to_weigh, create_penalty), create_weighted_graph)
