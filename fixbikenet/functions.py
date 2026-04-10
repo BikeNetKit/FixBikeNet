0865b9d7fadfa5b59a5c977559c368c54146a87d
Manuel-Knepper <manuel.knepper@gmx.net>
2026-04-10 14:52:43 +0200

    initial commit

---
+++ fixbikenet/functions.py
@@ -0,0 +1,257 @@
+import yaml
+import networkx as nx
+import random
+import numpy as np
+
+def map_edges_to_bike_infrastructure(g):
+    """
+    map if edges in graph have bike infrastructure as specified in config_osm.yml
+
+    Parameters
+    ----------
+    g :networkx.MultiDiGraph
+        simplified graph representing the street network
+
+    Returns
+    -------
+    g : networkx.MultiDiGraph
+        simplified graph representing the street network, with added binary edge attribute "pbi"
+    """
+    # first step: map all highway attributes
+    protected_bike_infra = yaml.load(
+        open("./config/config_osm.yml"),
+        Loader=yaml.FullLoader)["protected_bike_infra"]
+
+    # add binary edge attribute "pbi" (protected bike infra: True/False)
+    for edge in g.edges(keys=True):
+        g.edges[edge]["pbi"] = protected_bike_infra[
+            g.edges[edge]["highway"]
+        ]
+    return g
+
+def find_edges_to_drop(g):
+    """
+    find parallel edges that have different pbi values, list the ones with pbi=0
+
+    Parameters
+    ----------
+    g : networkx.MultiDiGraph
+        simplified graph representing the street network, with added binary edge attribute "pbi"
+
+    Returns
+    -------
+    edges_to_drop: list
+        unique list of edges to drop-> edges where pbi values differ and pbi value=0 gets dropped
+    """
+    # to find parallel edges, get all u,v tuples for which u,v,w>0 exists:
+    uvs = [edge[:2] for edge in list(g.edges) if edge[2] > 0]  # >0 includes key=1, key=2, ...
+    uvs = list(set(uvs))
+    edges_to_drop = []
+
+    for uv in uvs:
+        # collect all parallel edges for u-v node pair;
+        # account for the fact that edges are directed! uv[::-1]==vu might also be on the list
+        parallel_edges = [edge for edge in list(g.edges) if (edge[:2] == uv) or (edge[:2] == uv[::-1])]
+
+        # get set of PBIs for this u-v parallel edge list
+        pbis = set([g.edges[e]["pbi"] for e in parallel_edges])
+
+        # if we have both pbi==0 and pbi==1,
+        if len(pbis) == 2:
+            # add edges with pbi==0 to edges_to_drop list
+            to_drop = [e for e in parallel_edges if g.edges[e]["pbi"] == 0]
+            edges_to_drop += to_drop
+
+    edges_to_drop = list(set(edges_to_drop))
+    return edges_to_drop
+
+def weigh_edges(G, penalty):
+    """
+    adds weight parameter to all edges in G, which is calculated by multiplying the length of the edge with the corresponding penalty value
+
+    Parameters
+    ----------
+    G: networkx.Graph
+        undirected simple graph representing the street network
+    penalty: dictionary
+        dictionary of penalty values, dependent on if edge has bike infrastructure or not
+
+    Returns
+    -------
+    G: networkx.Graph
+        undirected simple graph representing the street network with weighted edges
+    """
+    for edge in G.edges:
+        # compute edge weight
+        edge_pbi = G.edges[edge]["pbi"]
+        edge_length = G.edges[edge]["length"]
+        edge_weight = edge_length * penalty[edge_pbi]
+        # add as attribute
+        G.edges[edge]["weight"] = edge_weight
+    return G
+
+def find_contact_nodes(G):
+    """
+    find nodes that have both edges with protected and without protected bike infrastructure incident on them
+
+    Parameters
+    ----------
+    G:networkx.Graph
+        undirected simple graph representing the street network with weighted edges
+
+    Return
+    ------
+    contact_nodes: list
+        list of all nodes that fulfill criteria to be a contact node
+    """
+    contact_nodes = []
+    for node in G.nodes:
+        pbis = set([G.edges[edge]["pbi"] for edge in G.edges(node)])
+        if len(pbis) == 2:
+            contact_nodes.append(node)
+    return contact_nodes
+
+def find_potential_gaps(contact_nodes, nodes_gdf, maxgap):
+    """
+    finds potential gaps in protected bicycle network, corresponding to two contact nodes that are within maxgap euclidean distance of each other
+
+    Parameters
+    ----------
+    contact_nodes: list
+        list of all nodes that fulfill criteria to be a contact node
+    nodes_gdf: geopandas.GeoDataFrame
+        all nodes in street network
+    maxgap: int
+        user defined maximal euclidean distance between two contact nodes
+
+    Returns
+    -------
+    potential_gaps: list
+        all unique potential gaps in protected bicycle network
+    """
+    potential_gaps = []
+
+    for node in contact_nodes:
+        node_buffer = nodes_gdf.loc[node, "geometry"].buffer(maxgap)
+        q = nodes_gdf.sindex.query(node_buffer, predicate="intersects")
+        neighbours = list(nodes_gdf.iloc[q].index)
+        neighbours.remove(node)
+        # convention: sort by ascending OSMID...
+        node_pairs = [tuple(sorted(z)) for z in zip([node] * len(neighbours), neighbours)]
+        potential_gaps += node_pairs
+
+    # ... so that we can easily deduplicate
+    potential_gaps = list(set(potential_gaps))
+    return potential_gaps
+
+def find_actual_gaps(G, potential_gaps):
+    """
+    determines which potential gaps are actual gaps by finding paths between all contact nodes and only keeping the gaps that have no protected bike infrastructure
+
+    Parameters
+    ----------
+    G: networkx.Graph
+        undirected simple graph representing the street network with weighted edges
+    potential_gaps: list
+        all unique potential gaps in protected bicycle network
+
+    Returns
+    -------
+    found_gaps: list
+        list of all gaps in protected bicycle network
+    found_gaps_nsp: list
+        list of paths in network for all gaps in protected bicycle network
+    """
+    pbi_dict = nx.get_edge_attributes(G, "pbi")
+
+    found_gaps = []
+    found_gaps_nsp = []  # naive shortest paths (by length, in node list format)
+
+    for i, gap in enumerate(potential_gaps):
+        u, v = gap
+        nodelist = nx.shortest_path(
+            G=G,
+            source=u,
+            target=v,
+            weight="length"
+        )
+        pbis = set([pbi_dict[tuple(sorted(z))] for z in zip(nodelist, nodelist[1:])])
+
+        # confirm that it is an actual gap if it consists only of pbi==0 infra:
+        if pbis == set([0]):
+            found_gaps.append(gap)
+            found_gaps_nsp.append(nodelist)
+    return found_gaps, found_gaps_nsp
+
+def compute_local_betweenness_centrality(G, nodes_gdf, radius):
+    """
+    computes weighted betweenness centrality for paths within radius
+
+    Parameters
+    ----------
+    G: networkx.Graph
+        undirected simple graph representing the street network with weighted edges
+    nodes_gdf: geopandas.GeoDataFrame
+        all nodes in street network
+    radius: int
+        maximum length of path for betweennessn centrality calculation, set by user
+
+    Returns
+    -------
+    ebc: dict
+        local betweenness centrality values for all edges in network
+    """
+    # set current ebc value of all G edges to 0
+    for edge in G.edges:
+        G.edges[edge]["ebc"] = 0
+
+    # create dict that will be updated at each step
+    ebc = nx.get_edge_attributes(G, "ebc")
+
+    # for each node, compute "local" ebc (buffered with radius!)
+    # for comp feas, now only subset of randomly drawn 100 nodes
+    random.seed(1312)
+    random_nodes = random.choices(list(G.nodes), k=100)
+    for node in random_nodes:
+        node_buffer = nodes_gdf.loc[node, "geometry"].buffer(radius)
+        q = nodes_gdf.sindex.query(node_buffer, predicate="intersects")
+        neighbours = list(nodes_gdf.iloc[q].index)
+        local_ebc = nx.edge_betweenness_centrality_subset(
+            G=G,
+            sources=[node],
+            targets=neighbours,
+            normalized=False,  # important! otherwise the addition makes no sense
+            weight="weight"  # using penalty for non-pbi
+        )
+
+        # update ebc dictionary
+        for k, v in local_ebc.items():
+            ebc[k] += v  # updating ebc!!
+    return ebc
+
+def rank_gaps_by_b(found_gaps_nsp, G, ebc):
+    """
+    calculates b for all gaps
+
+    Parameters
+    ----------
+    found_gaps_nsp: list
+        list of paths in network for all gaps in protected bicycle network
+    G: networkx.Graph
+        undirected simple graph representing the street network with weighted edges
+    ebc: dict
+        local betweenness centrality values for all edges in network
+
+    Returns
+    -------
+    Bs: list
+        list of values of b for all gaps in protected bicycle network
+    """
+    Bs = []
+    for nodelist in found_gaps_nsp:
+        edgelist = [tuple(sorted(z)) for z in zip(nodelist, nodelist[1:])]
+        lengths = np.array([G.edges[edge]["length"] for edge in edgelist])
+        ebcs = np.array([ebc[edge] for edge in edgelist])
+        B = sum(lengths * ebcs) / sum(lengths)
+        Bs.append(B)
+    return Bs
---
+++ tests/test_functions.py
@@ -0,0 +1,169 @@
+import pytest
+from networkx.utils.misc import graphs_equal
+import geopandas as gpd
+from shapely.geometry import Point
+
+from fixbikenet.functions import *
+
+@pytest.fixture
+def create_test_graph():
+    G = nx.MultiDiGraph()
+    G.add_edges_from([(1,2), (1,3), (2,3)])
+    highway_values = {(1,2,0):{'highway':'living_street'},
+                      (1,3,0):{'highway': 'motorway'},
+                      (2,3,0):{'highway':'path'}}
+    nx.set_edge_attributes(G,highway_values)
+    return G
+
+@pytest.fixture
+def create_validation_graph():
+    G = nx.MultiDiGraph()
+    G.add_edges_from([(1, 2), (1, 3), (2, 3)])
+    attributes = {(1,2,0): {'highway': 'living_street', 'pbi': True},
+                  (1,3,0): {'highway': 'motorway', 'pbi': False},
+                  (2,3,0): {'highway': 'path', 'pbi': True}}
+    nx.set_edge_attributes(G, attributes)
+    return G
+
+def test_edge_infra_mapping(create_test_graph, create_validation_graph):
+    assert graphs_equal(map_edges_to_bike_infrastructure(create_test_graph), create_validation_graph)
+
+@pytest.fixture
+def create_parallel_test_graph():
+    G = nx.MultiDiGraph()
+    G.add_edges_from([(1, 2),(1,2), (1, 3), (2, 3)])
+    attributes = {(1, 2, 0): {'highway': 'living_street', 'pbi': True},
+                  (1, 2, 1): {'highway': 'living_street', 'pbi': False},
+                  (1, 3, 0): {'highway': 'motorway', 'pbi': False},
+                  (2, 3, 0): {'highway': 'path', 'pbi': True}}
+    nx.set_edge_attributes(G, attributes)
+    return G
+
+@pytest.fixture
+def create_validation_edge_list():
+    edge_list = [(1, 2, 1)]
+    return edge_list
+
+def test_find_edges_to_drop(create_parallel_test_graph, create_validation_edge_list):
+    assert find_edges_to_drop(create_parallel_test_graph) == create_validation_edge_list
+
+@pytest.fixture
+def create_graph_to_weigh():
+    G = nx.Graph()
+    G.add_edges_from([(1, 2), (1, 3), (2, 3)])
+    attributes = {(1, 2): {'length': 10, 'pbi': True},
+                  (1, 3): {'length': 4, 'pbi': False},
+                  (2, 3): {'length': 5, 'pbi': True}}
+    nx.set_edge_attributes(G, attributes)
+    return G
+
+@pytest.fixture
+def create_weighted_graph():
+    G = nx.Graph()
+    G.add_edges_from([(1, 2), (1, 3), (2, 3)])
+    attributes = {(1, 2): {'length': 10, 'pbi': True, 'weight': 10},
+                  (1, 3): {'length': 4, 'pbi': False, 'weight': 20},
+                  (2, 3): {'length': 5, 'pbi': True, 'weight': 5}}
+    nx.set_edge_attributes(G, attributes)
+    return G
+
+@pytest.fixture
+def create_penalty():
+    penalty = {
+        0: 5,
+        1: 1
+    }
+    return penalty
+
+def test_weigh_edges(create_graph_to_weigh, create_weighted_graph, create_penalty):
+    assert graphs_equal(weigh_edges(create_graph_to_weigh, create_penalty), create_weighted_graph)
+
+@pytest.fixture
+def create_validation_contact_nodes():
+    contact_nodes = [1,3]
+    return contact_nodes
+
+def test_find_contact_nodes(create_weighted_graph, create_validation_contact_nodes):
+    assert find_contact_nodes(create_weighted_graph) == create_validation_contact_nodes
+
+@pytest.fixture
+def create_test_contact_nodes():
+    contact_nodes = [1,3,4]
+    return contact_nodes
+
+@pytest.fixture
+def create_maxgap():
+    maxgap = 50
+    return maxgap
+
+@pytest.fixture
+def create_test_nodes_gdf():
+    nodes ={'osmid':[1,2,3,4],'geometry':[Point(1,1), Point(1000,1000), Point(3,3), Point(2000,2000)]}
+    nodes_gdf = gpd.GeoDataFrame(nodes)
+    nodes_gdf.set_index('osmid',inplace=True)
+    return nodes_gdf
+
+@pytest.fixture
+def create_validation_gaps():
+    gaps = [(1,3)]
+    return gaps
+
+def test_find_potential_gaps(create_test_contact_nodes, create_maxgap, create_test_nodes_gdf, create_validation_gaps):
+    assert find_potential_gaps(create_test_contact_nodes,create_test_nodes_gdf, create_maxgap) == create_validation_gaps
+
+@pytest.fixture
+def create_graph_for_routing():
+    G = nx.Graph()
+    G.add_edges_from([(1, 2), (1, 3), (2, 3), (3,4)])
+    attributes = {(1, 2): {'length': 10, 'pbi': True, 'weight': 10},
+                  (1, 3): {'length': 4, 'pbi': False, 'weight': 20},
+                  (2, 3): {'length': 5, 'pbi': True, 'weight': 5},
+                  (3, 4): {'length': 6, 'pbi': False, 'weight': 30}}
+    nx.set_edge_attributes(G, attributes)
+    return G
+
+@pytest.fixture
+def create_potential_gaps():
+    potential_gaps = [(1,4), (2,3), (3,4)]
+    return potential_gaps
+
+@pytest.fixture
+def create_validation_found_gap_paths():
+    found_gap_paths = ([(1,4), (3,4)], [[1,3,4],[3,4]])
+    return found_gap_paths
+
+def test_find_actual_gaps(create_graph_for_routing, create_potential_gaps, create_validation_found_gap_paths):
+    assert find_actual_gaps(create_graph_for_routing, create_potential_gaps) == create_validation_found_gap_paths
+
+@pytest.fixture
+def create_betweenness_nodes():
+    nodes = {'osmid': [1, 2, 3, 4], 'geometry': [Point(1, 1), Point(5000, 5000), Point(3, 3), Point(1000, 1000)]}
+    nodes_gdf = gpd.GeoDataFrame(nodes)
+    nodes_gdf.set_index('osmid', inplace=True)
+    return nodes_gdf
+
+@pytest.fixture
+def create_radius():
+    radius = 2000
+    return radius
+
+@pytest.fixture
+def create_validation_ebc():
+    ebc = {(1,2):54.5, (1,3): 0, (2,3): 54.5, (3,4): 56.5}
+    return ebc
+
+def test_compute_local_betweenness_centrality(create_graph_for_routing, create_betweenness_nodes, create_radius, create_validation_ebc):
+    assert compute_local_betweenness_centrality(create_graph_for_routing, create_betweenness_nodes, create_radius) == create_validation_ebc
+
+@pytest.fixture
+def create_found_paths():
+    found_paths = [(1,3,4),(3,4)]
+    return found_paths
+
+@pytest.fixture
+def create_validation_bs():
+    validation_bs = [33.9,56.5]
+    return validation_bs
+
+def test_rank_gaps_by_b(create_found_paths, create_graph_for_routing, create_validation_ebc, create_validation_bs):
+    assert rank_gaps_by_b(create_found_paths, create_graph_for_routing, create_validation_ebc) == create_validation_bs
