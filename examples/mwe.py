41ab705c9b740ed1b69728edff95935f7b5f955a
Michael Szell <michael@szell.net>
2026-05-06 08:42:13 +0200

    Create mwe.py

---
+++ examples/mwe.py
@@ -0,0 +1,10 @@
+"""Minimum working example of fixbikenet."""
+
+import fixbikenet as fbn
+
+gaps = fbn.fixbikenet(
+    city_name="Frederiksberg municipality",
+)
+
+
+# data is saved in current working directory, as gaps.gpkg
